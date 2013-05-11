c9882f0255ee2bde9542569fd8e9f8b9621d5954
Priyandiono <ifdion@gmail.com>
2013-05-11 12:47:16 +0700

    Changed name space

--- shopping-cart/template/checkout.php
+++ shopping-cart/template/checkout.php
@@ -1,4 +1,5 @@
 <form id="checkout" name="checkout" class="well form-horizontal" action="<?php echo admin_url('admin-ajax.php'); ?>" method="post" enctype="multipart/form-data">
+	<h4><?php _e('Shipping Detail', 'cell-store') ?></h4>
 	<?php
 		if (is_user_logged_in()){
 			$update = true;
@@ -12,25 +13,25 @@
 		}
 	?>
 	<div class="control-group">
-		<label class="control-label" for="first-name"><?php _e('First Name', 'hijabchic') ?></label>
+		<label class="control-label" for="first-name"><?php _e('First Name', 'cell-store') ?></label>
 		<div class="controls">
 			<input type="text" class="input-xlarge " id="first-name" name="first-name" value="<?php if (isset($first_name)) {echo $first_name;} ?>">
 		</div>
 	</div>
 	<div class="control-group">
-		<label class="control-label" for="last-name"><?php _e('Last Name', 'hijabchic') ?></label>
+		<label class="control-label" for="last-name"><?php _e('Last Name', 'cell-store') ?></label>
 		<div class="controls">
 			<input type="text" class="input-xlarge " id="last-name" name="last-name" value="<?php if (isset($user_meta['last_name'][0])) {echo $user_meta['last_name'][0];} ?>">
 		</div>
 	</div>
 	<div class="control-group">
-		<label class="control-label" for="email"><?php _e('Email', 'hijabchic') ?></label>
+		<label class="control-label" for="email"><?php _e('Email', 'cell-store') ?></label>
 		<div class="controls">
 			<input type="text" class="input-xlarge " id="email" name="email" value="<?php if (isset($current_user->user_email)) {echo $current_user->user_email;}  ?>">
 		</div>
 	</div>
 	<div class="control-group">
-		<label class="control-label" for="telephone"><?php _e('Telephone', 'hijabchic') ?></label>
+		<label class="control-label" for="telephone"><?php _e('Telephone', 'cell-store') ?></label>
 		<div class="controls">
 			<?php
 				if (isset($user_meta['have-shipping'][0]) && isset($user_meta['shipping-telephone'][0])) {
@@ -45,7 +46,7 @@
 		</div>
 	</div>
 	<div class="control-group">
-		<label class="control-label" for="company"><?php _e('Company', 'hijabchic') ?></label>
+		<label class="control-label" for="company"><?php _e('Company', 'cell-store') ?></label>
 		<div class="controls">
 			<?php
 				if (isset($user_meta['have-shipping'][0]) && isset($user_meta['shipping-company'][0])) {
@@ -60,7 +61,7 @@
 		</div>
 	</div>
 	<div class="control-group">
-		<label class="control-label" for="address"><?php _e('Address', 'hijabchic') ?></label>
+		<label class="control-label" for="address"><?php _e('Address', 'cell-store') ?></label>
 		<div class="controls">
 			<?php
 				if (isset($user_meta['have-shipping'][0]) && isset($user_meta['shipping-address'][0])) {
@@ -75,7 +76,7 @@
 		</div>
 	</div>
 	<div class="control-group">
-		<label class="control-label" for="country"><?php _e('Country', 'hijabchic') ?></label>
+		<label class="control-label" for="country"><?php _e('Country', 'cell-store') ?></label>
 		<div class="controls">
 			<?php
 				$args = array(
@@ -99,7 +100,7 @@
 			?>
 			<select id="country" name="country" class="select-address" data-target="province">
 				<?php if ( $countries->have_posts() ) : ?>
-					<option value="intro"><?php _e('Please select', 'hijabchic') ?></option>
+					<option value="intro"><?php _e('Please select', 'cell-store') ?></option>
 					<?php while ( $countries->have_posts() ) : $countries->the_post(); ?>
 						<option value="<?php the_ID() ?>" <?php if(isset($selected_country)){selected($selected_country, get_the_ID());} ?>><?php the_title() ?></option>
 					<?php endwhile; ?>
@@ -108,7 +109,7 @@
 		</div>
 	</div>
 	<div class="control-group">
-		<label class="control-label" for="province"><?php _e('Province', 'hijabchic') ?></label>
+		<label class="control-label" for="province"><?php _e('Province', 'cell-store') ?></label>
 		<div class="controls">
 			<?php
 				if (isset($user_meta['have-shipping'][0]) && isset($user_meta['shipping-province'][0])) {
@@ -141,7 +142,7 @@
 				}
 			?>
 			<select id="province" name="province" <?php echo $disabled; ?> class="select-address" data-target="city">
-				<option value=""><?php _e('Please select', 'hijabchic') ?></option>
+				<option value=""><?php _e('Please select', 'cell-store') ?></option>
 				<?php if ( isset($provinces) && $provinces->have_posts() ) : ?>
 					<?php while ( $provinces->have_posts() ) : $provinces->the_post(); ?>
 						<option value="<?php the_ID() ?>" <?php selected($selected_province, get_the_ID()) ?>><?php the_title() ?></option>
@@ -151,7 +152,7 @@
 		</div>
 	</div>
 	<div class="control-group">
-		<label class="control-label" for="city"><?php _e('City', 'hijabchic') ?></label>
+		<label class="control-label" for="city"><?php _e('City', 'cell-store') ?></label>
 		<div class="controls">
 			<?php
 				if (isset($user_meta['have-shipping'][0]) && isset($user_meta['shipping-city'][0])) {
@@ -181,7 +182,7 @@
 				}
 			?>
 			<select id="city" name="city" <?php echo $disabled; ?> class="select-address" data-target="district">
-				<option value=""><?php _e('Please select', 'hijabchic') ?></option>
+				<option value=""><?php _e('Please select', 'cell-store') ?></option>
 				<?php if ( isset($cities) && $cities->have_posts() ) : ?>
 					<?php while ( $cities->have_posts() ) : $cities->the_post(); ?>
 						<option value="<?php the_ID() ?>" <?php selected($selected_city, get_the_ID()) ?>><?php the_title() ?></option>
@@ -191,7 +192,7 @@
 		</div>
 	</div>
 	<div class="control-group">
-		<label class="control-label" for="district"><?php _e('District', 'hijabchic') ?></label>
+		<label class="control-label" for="district"><?php _e('District', 'cell-store') ?></label>
 		<div class="controls">
 			<?php
 				if (isset($user_meta['have-shipping'][0]) && isset($user_meta['shipping-district'][0])) {
@@ -219,7 +220,7 @@
 				}
 			?>
 			<select id="district" name="district" <?php echo $disabled; ?> class="select-address">
-				<option value=""><?php _e('Please select', 'hijabchic') ?></option>
+				<option value=""><?php _e('Please select', 'cell-store') ?></option>
 				<?php if ( isset($selected_district) && isset($districts) && $districts->have_posts() ) : ?>
 					<?php while ( $districts->have_posts() ) : $districts->the_post(); ?>
 						<option value="<?php the_ID() ?>" <?php selected($selected_district, get_the_ID()) ?>><?php the_title() ?></option>
@@ -229,7 +230,7 @@
 		</div>
 	</div>
 	<div class="control-group">
-		<label class="control-label" for="postcode"><?php _e('Postcode', 'hijabchic') ?></label>
+		<label class="control-label" for="postcode"><?php _e('Postcode', 'cell-store') ?></label>
 		<div class="controls">
 			<?php
 				if (isset($user_meta['shipping-postcode'][0]) && $user_meta['shipping-postcode'][0] != '') {
@@ -246,27 +247,27 @@
 	<?php if (isset($update)): ?>
 		<div class="control-group">
 			<div class="controls">
-				<label for="save-shipping-address"><input type="checkbox" class="input-xlarge " id="save-shipping-address" name="save-shipping-address" value="1"> <?php _e('Save as Shipping Address', 'hijabchic') ?></label>
+				<label for="save-shipping-address"><input type="checkbox" class="input-xlarge " id="save-shipping-address" name="save-shipping-address" value="1"> <?php _e('Save as Shipping Address', 'cell-store') ?></label>
 			</div>
 		</div>
 	<?php else: ?>
-		<h4><?php _e('Set Up an Account', 'hijabchic') ?></h4>
+		<h4><?php _e('Set Up an Account', 'cell-store') ?></h4>
 		<div class="control-group">
-			<label class="control-label" for="username"><?php _e('Username', 'hijabchic') ?></label>
+			<label class="control-label" for="username"><?php _e('Username', 'cell-store') ?></label>
 			<div class="controls">
 				<input type="text" class="input-xlarge " id="username" name="username" value="">
 			</div>
 		</div>
 		<div class="control-group">
-			<label class="control-label" for="password"><?php _e('Password', 'hijabchic') ?></label>
+			<label class="control-label" for="password"><?php _e('Password', 'cell-store') ?></label>
 			<div class="controls">
 				<input type="password" class="input-xlarge " id="password" name="password" value="">
 			</div>
-			<p class="help-block"><?php _e('Checkout faster and track your purchase with your account.', 'hijabchic') ?></p>
+			<p class="help-block"><?php _e('Checkout faster and track your purchase with your account.', 'cell-store') ?></p>
 		</div>
 	<?php endif ?>
 	<div class="form-actions">
-		<button type="submit" class="btn btn-primary"><?php _e('Checkout', 'hijabchic') ?> <i class="icon icon-chevron-right icon-white"></i></button>
+		<button type="submit" class="btn btn-primary"><?php _e('Checkout', 'cell-store') ?> <i class="icon icon-chevron-right icon-white"></i></button>
 		<?php wp_nonce_field('checkout','checkout_nonce'); ?>
 		<input name="action" value="checkout" type="hidden">
 	</div>
