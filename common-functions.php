c8a2e462bc77d5728d93cabac700c9c3754dd745
Priyandiono <ifdion@gmail.com>
2013-05-11 07:10:54 +0700

    Moved global function from cell-store.php, Added 'get_template_file' wrapper function, By default shows admin bar only for admin,

--- common-functions.php
+++ common-functions.php
@@ -19,55 +19,73 @@ if (!function_exists('ajax_request')) {
 
 if (!function_exists('ajax_response')) {
 	function ajax_response($data,$redirect = false){
-			if(ajax_request()){
-				$data_json = json_encode($data);
-				echo $data_json;			
-			} else {
-				$_SESSION['global_message'][] = $data;
-			}
-			if ($redirect) {
-				wp_redirect( $redirect );
-				exit;
-				die();
-			}
+		if(ajax_request()){
+			$data_json = json_encode($data);
+			echo $data_json;			
+		} else {
+			$_SESSION['global_message'][] = $data;
+		}
+		if ($redirect) {
+			wp_redirect( $redirect );
+			exit;
+			die();
+		}
 	}	
+}
+
+/* global message 
+---------------------------------------------------------------
+*/
+
+add_action( 'init', 'setup_global_message');
+
+if (!function_exists('setup_global_message')) {
+	function setup_global_message(){
+		global $global_message;
+		if ( isset( $_SESSION['global_message'] ) ){
+			$global_message = $_SESSION['global_message'];
+			unset( $_SESSION['global_message'] );
+		}
+	}
 }
 
 if (!function_exists('the_global_message')) {
 	function the_global_message(){
-
 		global $global_message;
-
 		if ($global_message != '' && (count($global_message) > 0)) {
 			foreach ($global_message as $message){
 				?>
-					<div id="" class="alert alert-<?php echo $message['type'] ?>">
+					<div id="" class="alert alert-<?php echo $message['type'].' '.$message['type'] ?>">
 						<a href="" class="delete">✕</a> <span><?php echo $message['message'] ?></span>
 					</div>
 				<?php
 			}
 		}
+		$global_message = false;
 	}	
 }
 
-/* wp-admin : remove admin bar
+
+/* get template file 
+---------------------------------------------------------------
+*/
+if (!function_exists('get_template_file')) {
+	function get_template_file($template){
+		ob_start();
+			include($template);
+			$shopping_cart_content = ob_get_contents();
+		ob_end_clean();
+		return $shopping_cart_content;
+	}
+}
+
+
+/* admin_bar_for_admin_only
 ---------------------------------------------------------------
 */
 
-// add_action('init', 'remove_admin_bar');
-
-// function remove_admin_bar() {
-// 	if (!current_user_can('administrator') && !is_admin()) {
-// 		show_admin_bar(false);
-// 	}
-// }
-
-
-
 if (function_exists('admin_bar_for_admin_only')) {
-
 	add_filter( 'show_admin_bar' , 'admin_bar_for_admin_only');
-
 	function admin_bar_for_admin_only(){
 		if (!current_user_can('administrator') && !is_admin()) {
 			return false;
@@ -78,36 +96,32 @@ if (function_exists('admin_bar_for_admin_only')) {
 }
 
 
-
-/* wp-admin : disable non administrator to access wp-admin
----------------------------------------------------------------
-*/
-function my_admin_init(){
-	if( !defined('DOING_AJAX') && !current_user_can('administrator') ){
-		wp_redirect( home_url() );
-		exit();
-	}
-}
-add_action('admin_init','my_admin_init');
-
-
 /* wp-admin : add script in wp-admin 
 ---------------------------------------------------------------
 */
-add_action('admin_print_scripts', 'add_script'); //dion
-function add_script() { //dion
-	wp_enqueue_script( 'jquery-ui-datepicker' );
-	wp_enqueue_script('admin-cellscript', plugins_url() . '/cell-store/js/admin-script.js', array('jquery'),'1.0',true);
+
+if (!function_exists('cell_store_admin_script')) {
+	add_action('admin_print_scripts', 'cell_store_admin_script'); //dion
+
+	function cell_store_admin_script() { //dion
+		wp_enqueue_script( 'jquery-ui-datepicker' );
+		wp_enqueue_script('admin-cellscript', plugins_url() . '/cell-store/js/admin-script.js', array('jquery'),'1.0',true);
+	}
 }
+
 
 
 /* wp-admin : add style in wp-admin 
 ---------------------------------------------------------------
 */
-add_action('admin_enqueue_scripts', 'add_style');
-function add_style() {
-	wp_enqueue_style('smoothness', plugins_url() . '/cell-store/css/smoothness/jquery-ui-1.9.0.custom.min.css');
+
+if (!function_exists('cell_store_admin_style')) {
+	add_action('admin_enqueue_scripts', 'cell_store_admin_style');
+	function cell_store_admin_style() {
+		wp_enqueue_style('smoothness', plugins_url() . '/cell-store/css/smoothness/jquery-ui-1.9.0.custom.min.css');
+	}
 }
+
 
 /* wp-admin : Adding Custom Post Type and Custom Taxonomy to Right Now Admin Widget
 --------------------------------------------------------------
@@ -155,11 +169,11 @@ function ucc_right_now_content_table_end() {
 /* wp-admin : add small image  size for admin table
 ---------------------------------------------------------------
 */
-if ( function_exists( 'add_theme_support' ) ) {
+if (function_exists( 'add_theme_support' ) ) {
 	add_theme_support( 'post-thumbnails' );
 }
 
-if ( function_exists( 'add_image_size' ) ) { 
+if (function_exists( 'add_image_size' ) ) { 
 	add_image_size( '50', 50, 50, true );
 }
 
@@ -167,7 +181,7 @@ if ( function_exists( 'add_image_size' ) ) {
 ---------------------------------------------------------------
 */
 
-if(function_exists('is_descendant')){
+if(!function_exists('is_descendant')){
 	function is_descendant( $page, $ancestor = false ) {
 		if( !is_object( $page ) ) {
 			$page = intval( $page );
@@ -189,7 +203,7 @@ if(function_exists('is_descendant')){
 /* get id from slug 
 ---------------------------------------------------------------
 */
-if (function_exists('get_id_by_slug')) {
+if (!function_exists('get_id_by_slug')) {
 	function get_id_by_slug($post_slug,$post_type){
 		global $wpdb;
 		$post_id = $wpdb->get_var(
