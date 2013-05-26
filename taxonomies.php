c45d35a2f16a178f0b316ab9b832637e8f321311
Priyandiono <ifdion@gmail.com>
2013-05-26 21:41:06 +0700

    Collextion list shortcode

--- taxonomies.php
+++ taxonomies.php
@@ -101,3 +101,176 @@ function cell_store_taxonomies() {
 	));
 
 }
+
+
+/* create taxonomy for every project
+---------------------------------------------------------------
+*/
+
+function save_cpt_as_term($post_id) {
+
+	$post = get_post( $post_id );
+	$cpt_as_term = array('collection');
+
+	
+	if(in_array($post->post_type, $cpt_as_term) && $post->post_status=='publish'){
+
+		$term_args['name'] = $post->post_title;
+		$term_args['slug'] = $post->post_name.'';
+		$term_id = get_post_meta($post_id, $post->post_type.'-term-id', true);
+
+		if($term_id){
+			$term = wp_update_term( $term_id, $post->post_type.'-term', $term_args );
+		} else {
+			$term = wp_insert_term( $term_args['name'], $post->post_type.'-term', $term_args );
+			$meta_status = add_post_meta($post_id, $post->post_type.'-term-id', $term['term_id'], true);
+		}
+
+	}
+}
+// add_action( 'save_post', 'save_cpt_as_term');
+
+
+/* delete taxonomy on post delete
+---------------------------------------------------------------
+*/
+// add_action('admin_init', 'codex_init');
+function codex_init() {
+	global $wpdb;
+	if (current_user_can('delete_posts')) add_action('before_delete_post', 'delete_cpt_as_term', 1);
+}
+
+function delete_cpt_as_term($post_id) {
+	$cpt_as_term = array('collection');
+	$post = get_post( $post_id );
+	if (in_array($post->post_type, $cpt_as_term)) {
+		$term = get_post_meta($post_id, $post->post_type.'-term-id', true);
+		wp_delete_term( $term, $post->post_type.'-term');
+	}
+}
+
+/* cell store collection 
+---------------------------------------------------------------
+*/
+
+add_shortcode( 'cell-store-collection', 'cell_store_collection' );
+
+function cell_store_collection(){
+
+
+	$current_paging = get_query_var( 'paged' );
+
+	$taxonomies = array( 
+		'collection'
+	);
+
+	$per_page = 12;
+
+	if ($current_paging == 0) {
+		$offset = 0;
+	} else {
+		$offset = ($current_paging-1) * $per_page;	
+	}
+	
+	$taxonomies = array( 
+		'collection'
+	);
+
+	$args = array(
+		'orderby'       => 'id', 
+		'hide_empty'    => true, 
+		'number'        => $per_page, 
+		'fields'        => 'all', 
+		'offset'        => $offset, 
+	); 
+
+	$collections = get_terms( $taxonomies, $args );
+
+	$all_collections = wp_count_terms($taxonomies);
+
+	ob_start();
+
+		foreach ($collections as $key => $collection) {
+			$link = get_term_link( $collection );
+
+			$args = array(
+				'post_type' => 'banner',
+				'posts_per_page' => 1,
+				'tax_query' => array(
+					array(
+						'taxonomy' => 'position',
+						'field' => 'slug',
+						'terms' => $collection->slug
+					)
+				)
+			);
+			$side_loop = new WP_query($args);
+
+			if ( $side_loop->have_posts() ) :
+				while ( $side_loop->have_posts() ) : $side_loop->the_post();
+					if (has_post_thumbnail( get_the_ID())) {
+						echo '<div id="" class="archive-banner">';
+						echo '<a href="'.$link.'">';
+							the_post_thumbnail('800');
+						echo '</a>';
+						echo '</div>';
+					}
+				endwhile;
+			endif;
+
+		}
+
+		if ($all_collections > count($collections)) {
+
+			$page = get_queried_object();
+			$page_link = get_permalink( $page->ID );
+			$total_page = ceil($all_collections / $per_page);
+			$current_paging = get_query_var( 'paged' );
+
+			if ($current_paging == 0 ) {
+
+				$next_link = add_query_arg('paged','2',$page_link);
+
+			} elseif ($current_paging == 2 ) {
+
+				$prev_link = $page_link;
+
+				$next_page = $current_paging + 1;
+				$next_link = add_query_arg('paged',$next_page,$page_link);
+
+			} elseif ($current_paging == $total_page ) {
+
+				$prev_page = $current_paging - 1;
+				$prev_link = add_query_arg('paged',$prev_page,$page_link);
+			} else {
+
+				$prev_page = $current_paging - 1;
+				$prev_link = add_query_arg('paged',$next_page,$page_link);
+
+				$next_page = $current_paging + 1;
+				$next_link = add_query_arg('paged',$next_page,$page_link);
+
+			}
+
+			if (isset($prev_link)) {
+				echo '<a href="'.$prev_link.'">Previous</a> ';
+			}
+
+			if (isset($next_link)) {
+				echo '<a href="'.$next_link.'">Next</a> ';
+			}
+
+
+			
+
+			// echo '<a href="#">Previous</a> ';
+			// echo ' <a href="'.$next_link.'">Next</a>';
+			// echo count($collections).'/'.$all_collections;
+		}
+
+		$collections_content = ob_get_contents();
+	ob_end_clean();
+
+	
+	return $collections_content;
+}
