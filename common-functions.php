9e7361af37528fd8fa65001fe16ce27e6e3973a3
Dion Priyandiono <ifdion@gmail.com>
2015-06-12 10:33:05 +0700

    remove duplicate functions

--- common-functions.php
+++ common-functions.php
@@ -4,97 +4,97 @@
 ---------------------------------------------------------------
 */
 
-if (!function_exists('ajax_request')) {
-	function ajax_request(){
-		if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-			return true;
-		} else {
-			return false;
-		}
-	}
-}
+// if (!function_exists('ajax_request')) {
+// 	function ajax_request(){
+// 		if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+// 			return true;
+// 		} else {
+// 			return false;
+// 		}
+// 	}
+// }
 
 /* ajax : custom function to create an ajax response or http location redirect
 ---------------------------------------------------------------
 */
 
-if (!function_exists('ajax_response')) {
-	function ajax_response($data,$redirect = false){
-		if(ajax_request()){
-			$data_json = json_encode($data);
-			echo $data_json;			
-		} else {
-			$_SESSION['global_message'][] = $data;
-		}
-		if ($redirect) {
-			wp_redirect( $redirect );
-			exit;
-			die();
-		}
-	}	
-}
+// if (!function_exists('ajax_response')) {
+// 	function ajax_response($data,$redirect = false){
+// 		if(ajax_request()){
+// 			$data_json = json_encode($data);
+// 			echo $data_json;			
+// 		} else {
+// 			$_SESSION['global_message'][] = $data;
+// 		}
+// 		if ($redirect) {
+// 			wp_redirect( $redirect );
+// 			exit;
+// 			die();
+// 		}
+// 	}	
+// }
 
 /* global message 
 ---------------------------------------------------------------
 */
 
-add_action( 'init', 'setup_global_message');
+// add_action( 'init', 'setup_global_message');
 
-if (!function_exists('setup_global_message')) {
-	function setup_global_message(){
-		global $global_message;
-		if ( isset( $_SESSION['global_message'] ) ){
-			$global_message = $_SESSION['global_message'];
-			unset( $_SESSION['global_message'] );
-		}
-	}
-}
+// if (!function_exists('setup_global_message')) {
+// 	function setup_global_message(){
+// 		global $global_message;
+// 		if ( isset( $_SESSION['global_message'] ) ){
+// 			$global_message = $_SESSION['global_message'];
+// 			unset( $_SESSION['global_message'] );
+// 		}
+// 	}
+// }
 
-if (!function_exists('the_global_message')) {
-	function the_global_message(){
-		global $global_message;
-		if ($global_message != '' && (count($global_message) > 0)) {
-			foreach ($global_message as $message){
-				?>
-					<div id="" class="alert alert-<?php echo $message['type'].' '.$message['type'] ?>">
-						<a href="" class="delete">✕</a> <span><?php echo $message['message'] ?></span>
-					</div>
-				<?php
-			}
-		}
-		$global_message = false;
-	}	
-}
+// if (!function_exists('the_global_message')) {
+// 	function the_global_message(){
+// 		global $global_message;
+// 		if ($global_message != '' && (count($global_message) > 0)) {
+// 			foreach ($global_message as $message){
+// 				? >
+// 					<div id="" class="alert alert-<?php echo $message['type'].' '.$message['type'] ? >">
+// 						<a href="" class="delete">✕</a> <span><?php echo $message['message'] ? ></span>
+// 					</div>
+// 				< ?php
+// 			}
+// 		}
+// 		$global_message = false;
+// 	}	
+// }
 
 
 /* get template file 
 ---------------------------------------------------------------
 */
-if (!function_exists('get_template_file')) {
-	function get_template_file($template){
-		ob_start();
-			include($template);
-			$shopping_cart_content = ob_get_contents();
-		ob_end_clean();
-		return $shopping_cart_content;
-	}
-}
+// if (!function_exists('get_template_file')) {
+// 	function get_template_file($template){
+// 		ob_start();
+// 			include($template);
+// 			$shopping_cart_content = ob_get_contents();
+// 		ob_end_clean();
+// 		return $shopping_cart_content;
+// 	}
+// }
 
 
 /* admin_bar_for_admin_only
 ---------------------------------------------------------------
 */
 
-if (function_exists('admin_bar_for_admin_only')) {
-	add_filter( 'show_admin_bar' , 'admin_bar_for_admin_only');
-	function admin_bar_for_admin_only(){
-		if (!current_user_can('administrator') && !is_admin()) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-}
+// if (function_exists('admin_bar_for_admin_only')) {
+// 	add_filter( 'show_admin_bar' , 'admin_bar_for_admin_only');
+// 	function admin_bar_for_admin_only(){
+// 		if (!current_user_can('administrator') && !is_admin()) {
+// 			return false;
+// 		} else {
+// 			return true;
+// 		}
+// 	}
+// }
 
 
 /* wp-admin : add script in wp-admin 
@@ -170,13 +170,13 @@ function ucc_right_now_content_table_end() {
 /* wp-admin : add small image  size for admin table
 ---------------------------------------------------------------
 */
-if (function_exists( 'add_theme_support' ) ) {
-	add_theme_support( 'post-thumbnails' );
-}
+// if (function_exists( 'add_theme_support' ) ) {
+// 	add_theme_support( 'post-thumbnails' );
+// }
 
-if (function_exists( 'add_image_size' ) ) { 
-	add_image_size( '50', 50, 50, true );
-}
+// if (function_exists( 'add_image_size' ) ) { 
+// 	add_image_size( '50', 50, 50, true );
+// }
 
 /* check if is descendant 
 ---------------------------------------------------------------
@@ -204,19 +204,19 @@ if(!function_exists('is_descendant')){
 /* get id from slug 
 ---------------------------------------------------------------
 */
-if (!function_exists('get_id_by_slug')) {
-	function get_id_by_slug($post_slug,$post_type){
-		global $wpdb;
-		$post_id = $wpdb->get_var(
-			"	SELECT ID
-				FROM wp_posts
-				WHERE post_name = '$post_slug'
-				AND post_type ='$post_type'
-				LIMIT 0,1
-			");
-		return $post_id;
-	}
-}
+// if (!function_exists('get_id_by_slug')) {
+// 	function get_id_by_slug($post_slug,$post_type){
+// 		global $wpdb;
+// 		$post_id = $wpdb->get_var(
+// 			"	SELECT ID
+// 				FROM wp_posts
+// 				WHERE post_name = '$post_slug'
+// 				AND post_type ='$post_type'
+// 				LIMIT 0,1
+// 			");
+// 		return $post_id;
+// 	}
+// }
 
 /* setup currency format 
 ---------------------------------------------------------------
