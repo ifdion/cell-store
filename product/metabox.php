7cc55373363d18ad561a1d0bde8dc9f7646d9086
Dion Priyandiono <ifdion@gmail.com>
2015-06-12 10:26:10 +0700

    update qpalchemy usage

--- product/metabox.php
+++ product/metabox.php
@@ -27,9 +27,10 @@
 			<span><?php _e('Enter the shipping weight', 'cell-store') ?></span>
 		</p>
 		<a style="float:right; margin:0 10px;" href="#" class="dodelete-detail button"><?php _e('Remove All', 'cell-store') ?></a>
+
 		<?php while($mb->have_fields_and_multi('detail')): ?>
-			<div id="" class="">
-				<?php $mb->the_group_open(); ?>
+			<?php $mb->the_group_open(); ?>
+				<div id="" class="">
 					<label><?php _e('Details & Description', 'cell-store') ?></label>
 					<p>
 						<input type="text" name="<?php $mb->the_name('title'); ?>" value="<?php $mb->the_value('title'); ?>"/>
@@ -37,26 +38,26 @@
 						<a href="#" class="dodelete button">✕</a>
 						
 					</p>
-				<?php $mb->the_group_close(); ?>
-			</div>
+				</div>
+			<?php $mb->the_group_close(); ?>
 		<?php endwhile; ?>
+
 		<p style="margin-bottom:15px; padding-top:5px;"><a href="#" class="docopy-detail button"><?php _e('Add Detail', 'cell-store') ?></a></p>
+
 	</div>
 	<div id="product-variation" class="meta-parts">
 		<h4><input type="checkbox" data-target="#variation-input" class="use-optional" name="<?php $metabox->the_name('use_variations'); ?>" value="1"<?php if ($metabox->get_the_value('use_variations')) echo ' checked="checked"'; ?>/> <?php _e('Use Product Options', 'cell-store') ?></h4>
 		<div id="variation-input" class="optional-meta">
 			<a style="float:right; margin:0 10px;" href="#" class="dodelete-variant button"><?php _e('Remove All', 'cell-store') ?></a>
 			<?php while($mb->have_fields_and_multi('variant')): ?>
-				<div id="" class="">
-					<?php $mb->the_group_open(); ?>
+				<?php $mb->the_group_open(); ?>
 						<label><?php _e('Option Name <span class="stock-management" > & Stock </span>', 'cell-store') ?></label>
 						<p>
 							<input type="text" name="<?php $mb->the_name('title'); ?>" value="<?php $mb->the_value('title'); ?>"/>
 							<input type="text" class="stock-management" name="<?php $mb->the_name('stock'); ?>" value="<?php $mb->the_value('stock'); ?>"/>
 							<a href="#" class="dodelete button">✕</a>
 						</p>
-					<?php $mb->the_group_close(); ?>
-				</div>
+				<?php $mb->the_group_close(); ?>
 			<?php endwhile; ?>
 			<p style="margin-bottom:15px; padding-top:5px;"><a href="#" class="docopy-variant button"><?php _e('Add Variant', 'cell-store') ?></a></p>
 		</div>
