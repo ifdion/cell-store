c7aa7c9ea20bc1c609414cb7ea3c4038fb0a0ac3
Dion Priyandiono <ifdion@gmail.com>
2015-07-03 15:39:44 +0700

    remove price, stock and name input

--- product/ajax.php
+++ product/ajax.php
@@ -7,64 +7,51 @@ add_action('wp_ajax_nopriv_add_to_cart', 'process_add_to_cart');
 add_action('wp_ajax_add_to_cart', 'process_add_to_cart');
 
 function process_add_to_cart() {
-	global $cell_store_option;
-	$shopping_cart_page = $cell_store_option['shopping']['page']['shopping-cart'];
-
 	if ( empty($_POST) || !wp_verify_nonce($_POST['add_to_cart_nonce'],'add_to_cart') ) {
 		echo 'Sorry, your nonce did not verify.';
 		die();
 	} else {
-		// validate data
-		$product_name = $_POST['product_name'];
+		// get pages
+		$cell_store_pages = get_option( 'cell_store_pages' );
+		$shopping_cart_page = $cell_store_pages['shopping-cart'];
+
+		// get post data
 		$product_id = $_POST['product_id'];
 		$quantity = $_POST['quantity'];
-		$stock = $_POST['stock'];
+		$return = $_POST['_wp_http_referer'];
+		
+		$cart_item = $product_id;
+		$product_option = false;
 		if (isset($_POST['product-option'])) {
 			$product_option = $_POST['product-option'];
+			$cart_item = $product_id.'-'.$product_option;
 		}
-		$weight = $_POST['weight'];
-		$price = $_POST['price'];
-		$return = $_POST['_wp_http_referer'];
 		if ($_POST['return']) {
 			$return = get_permalink(get_page_by_path($shopping_cart_page));
 		}
-		
-		// define the product key, stock and product option based on product id and option
-		if (isset($product_option) && isset($stock)) {
-			$cart_item = $product_id.'-'.$product_option;
-			$stock = $stock[$product_option];
-		} elseif($stock) {
-			$cart_item = $product_id;
-			$stock = $stock['main'];
-			$product_option = false;
-		} elseif($product_option) {
-			$cart_item = $product_id.'-'.$product_option;
-			$stock = false;
-		} else {
-			$cart_item = $product_id;
-			$product_option = false;
-			$stock = false;
-		}
 
-		// if stock is managed, add number of quantity based on session
-		if ($stock) {
-			if (isset($_SESSION['shopping-cart']['items'][$cart_item])) {
-				$quantity_in_cart = $_SESSION['shopping-cart']['items'][$cart_item]['quantity'];
-				if (($quantity + $quantity_in_cart) > $stock) {
-					$quantity = $stock;
-				} else {
-					$quantity = $quantity + $quantity_in_cart;
-				}
-			} else {
-				if ($quantity > $stock) {
-					$quantity = $stock;
+		// get from database
+		$product_data = get_post($product_id );
+		$product_meta = get_post_meta($product_id );
+		$product_name = $product_data->post_title;
+
+		// check stock
+		if (isset($product_meta['_use_stock_management'][0])) {
+			if (isset($product_meta['_stock'][0])) {
+				$stock = $product_meta['_stock'][0];
+			}
+			if (isset($product_meta['_use_variations'][0])) {
+				$product_variations = unserialize($product_meta['_variant'][0]);
+
+				foreach ($product_variations as $value) {
+					if ($value['title'] == $product_option) {
+						$stock = $value['stock'];
+					}
 				}
 			}
-		} else {
-			if ($_SESSION['shopping-cart']['items'][$cart_item]) {
-				$quantity_in_cart = $_SESSION['shopping-cart']['items'][$cart_item]['quantity'];
-				$quantity = $quantity + $quantity_in_cart;
-			}
+		}
+		if ($quantity > $stock) {
+			$quantity = $stock;
 		}
 
 		// set up new item data
@@ -83,13 +70,8 @@ function process_add_to_cart() {
 		unset($_SESSION['shopping-cart']['payment']['shipping-rate']);
 
 		$result['type'] = 'success';
-		$result['message'] = sprintf(__(' %s added to shopping-cart ','cell-store'), $product_name);
+		$result['message'] = sprintf(__(' %s added to Shopping Cart ','cell-store'), $product_name);
 		ajax_response($result,$return);
-
-
-		die();
 	}
 }
-
-
 ?>
